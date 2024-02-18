1e3369c811d24dcb6af7e885dbd88e05a109451b
Vipul Dixit <dixit.vipul0511@gmail.com>
2024-02-18 08:09:36 +0530

    server prompt

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,6 @@ app.use(cors());
 // Function to encrypt data
 const cryptr = new Cryptr("myTotallySecretKey");
 
-// const decryptedString = cryptr.decrypt(encryptedString);
-
-// console.log(encryptedString); // 2a3260f5ac4754b8ee3021ad413ddbc11f04138d01fe0c5889a0dd7b4a97e342a4f43bb43f3c83033626a76f7ace2479705ec7579e4c151f2e2196455be09b29bfc9055f82cdc92a1fe735825af1f75cfb9c94ad765c06a8abe9668fca5c42d45a7ec233f0
-// console.log(decryptedString);
-
 app.post("/encrypt", (req, res) => {
   const data = req.body.data;
   const encryptedString = cryptr.encrypt(data);
@@ -31,5 +26,5 @@ app.post("/decrypt", (req, res) => {
   console.log("Data Send");
 });
 app.listen(port, () => {
-  console.log("the server is Running");
+  console.log("The server is running!!");
 });
